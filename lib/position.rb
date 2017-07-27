ec58114cb94d37f7e83a63d1717073efe12370a6
Milo Vrach <morelo87@gmail.com>
2017-07-27 09:59:14 +0200

    Add move

--- lib/position.rb
+++ lib/position.rb
@@ -6,4 +6,15 @@ class Position
      @board = board || %w(-)*SIZE
      @turn = turn
   end
+  def move(idx)
+    position = Position.new(@board.dup, xturn("o", "x"))
+    position.board[idx] = turn
+    position
+
+  end
+  
+  def xturn(x, o)
+       turn == "x" ? x : o
+
+  end
 end
