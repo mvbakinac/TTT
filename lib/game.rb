aaa8c039ede1f4a714dc40bef21e58c35da6686f
Milo Vrach <morelo87@gmail.com>
2017-07-27 19:18:09 +0200

    Add grid

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,22 @@ require "./lib/position"
 
 
 class Game < Gosu::Window
-  attr_accessor :width
+  attr_accessor :width, :cell_width
 
   def initialize
-      width = 600
+      @width = 600
+      @cell_width = @width / 3
       super(width, width, false)
+  end
+  def needs_cursor?
+     true
+  end
+ 
+  def draw
+    [cell_width, cell_width * 2].each do |w|
+      draw_line(w, 0, Gosu::Color::WHITE, w, width, Gosu::Color::WHITE)
+      draw_line(0, w, Gosu::Color::WHITE, width, w, Gosu::Color::WHITE)
+    end
   end
 end
 
